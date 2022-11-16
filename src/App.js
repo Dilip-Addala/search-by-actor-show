ceda809cfabaf7ed8bcd6b2a0316f595c394f537
Dilip-Addala <addaladilip1998@gmail.com>
2022-11-16 19:15:21 +0530

    modified css nd functions

--- src/App.js
+++ src/App.js
@@ -11,24 +11,35 @@ function App() {
   const [input, setInput] = useState("");
   const [resultData, setResultData] = useState([]);
   const [submit, setSubmit] = useState(false);
+  const [flag, setFlag] = useState(false);
 
   const placeholder =
     category === "people" ? "Search by Actors Name" : "Search by Shows Name";
 
-  const fetchData = async (e) => {
-    e.preventDefault();
-    const url = `https://api.tvmaze.com/search/${category}?q=${input}`;
-    const response = await axios.get(url);
-    setResultData(response.data);
-    setSubmit(true);
+  const fetchData = async (event) => {
+    event.preventDefault();
+    if (input !== "") {
+      const url = `https://api.tvmaze.com/search/${category}?q=${input}`;
+      const response = await axios.get(url);
+      if (response.data.length > 0) {
+        setResultData(response.data);
+        setFlag(false);
+        setSubmit(true);
+      } else {
+        setFlag(true);
+      }
+    }
   };
 
+  console.log(flag);
+
   const changeType = (e) => {
-    const id = e.target.id
-    setCategory(id)
-    setSubmit(false)
-    setInput("")
-  }
+    const id = e.target.id;
+    setCategory(id);
+    setSubmit(false);
+    setInput("");
+    setFlag(false);
+  };
 
   return (
     <div className="h-screen flex justify-center">
@@ -69,10 +80,13 @@ function App() {
           </div>
         </div>
         {category && (
-          <form className="flex justify-center bg-slate-200 w-full " onSubmit={fetchData}>
+          <form
+            className="flex justify-center bg-slate-200 w-full "
+            onSubmit={fetchData}
+          >
             <Inputs
               placeholder={placeholder}
-              onChange={e=>setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
               value={input}
             />
             <button className="bg-blue-600 ml-3 p-2 cursor-pointer text-white font-bold rounded-md mb-3 ">
@@ -80,12 +94,15 @@ function App() {
             </button>
           </form>
         )}
-        {submit && (
-            <div className="flex bg-gradient-to-bl from-slate-500 to-black w-full flex-wrap mt-5 mb-5">
-            {resultData?.map((post) =><Cards details={post} category={category} key={post.id}/>)}
-            {!resultData&&<h1>No records found...</h1>}
-        </div>
+        {submit!==flag && (
+          // <div className="flex bg-gradient-to-bl from-slate-500 to-black w-full flex-wrap mt-5 mb-5">
+          <div className="flex  w-full flex-wrap mt-5 mb-5">
+            {resultData?.map((post) => (
+              <Cards details={post} category={category} key={post.id} />
+            ))}
+          </div>
         )}
+        {flag && <h1 className="text-3xl mt-5 font-semibold">No records...</h1>}
       </div>
     </div>
   );
--- src/Cards/cards.js
+++ src/Cards/cards.js
@@ -1,26 +1,41 @@
-import NoImage from "../noImage/Noimg.png"
+import NoImage from "../noImage/Noimg.png";
 
 const Cards = (props) => {
-  const { details,category } = props;
-  console.log(details)
-  let data
-  let id
+  const { details, category } = props;
+  let data;
 
-  if (category==="people"){
+  if (category === "people") {
     data = details?.person;
-    id = details?.person?.id;
-  }else if(category==="shows"){
+    const emptyImgCheck = details?.person?.image;
+    const emptyCountryCheck = details?.person?.country;
+    if (emptyImgCheck === null || emptyCountryCheck === null) {
+      return null;
+    }
+  } else if (category === "shows") {
     data = details?.show;
-    id = details?.show?.id;
+    const emptyImgCheck = details?.show?.image;
+    const emptySummaryCheck = details?.show?.summary;
+    const emptyPremierCheck = details?.show?.premiered;
+    if (
+      emptyImgCheck === null ||
+      emptySummaryCheck === "" ||
+      emptySummaryCheck === null ||
+      emptyPremierCheck === null
+    ) {
+      return null;
+    }
   }
-  
-  const image = data?.image?.medium ?  data?.image.medium : NoImage
+
+  //   const emptyCheck = data.img
+
+  const image = data?.image?.medium ? data?.image.medium : NoImage;
+  const rating = data?.rating?.average ? data.rating.average : "--";
 
   return (
-    <a key={id}
+    <a
       href={data?.url}
       target="blank"
-      className="w-1/5 ring-2 ring-slate ring-offset-white bg-white p-3 rounded-xl mr-5 ml-12 min-h-max overflow-hidden mb-5 mt-5 shadow-2xl"
+      className="w-1/5 ring-2 ring-slate ring-offset-white bg-white p-3 rounded-xl mr-5 ml-11 min-h-max overflow-hidden mb-5 mt-5 shadow-2xl"
     >
       <div className="flex flex-col items-center w-full border-3 ">
         {data?.name && (
@@ -29,14 +44,24 @@ const Cards = (props) => {
           </h1>
         )}
         <img src={image} alt={data?.name} className="w-72 h-52" />
+        {data?.premiered && (
+          <h1 className="text-2xl font-bold font-serif mb-3 mt-3 text-slate-600">
+            Premiered: {data.premiered}
+          </h1>
+        )}
+        {data?.country && (
+          <h1 className="text-xl font-bold font-serif mb-3 mt-3 text-blue-500">
+            Country: {data.country.name}
+          </h1>
+        )}
         {data.summary && (
-          <div className="h-52 overflow-hidden">
-            <p>{data.summary}</p>
+          <div className="h-52 overflow-scroll">
+            <div dangerouslySetInnerHTML={{ __html: data.summary }} />
           </div>
         )}
-        {data?.rating?.average && (
+        {data?.rating && (
           <p className="text-2xl font-bold font-serif mb-3 text-slate-600 mt-3 shadow-md">
-            Rating: {data.rating.average}
+            Rating: {rating}
           </p>
         )}
       </div>
